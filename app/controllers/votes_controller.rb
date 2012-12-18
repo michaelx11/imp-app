2fef48fe5a88f1acdd3111ebc6d1063defd37c4d
kyc <kyc2915@mit.edu>
2012-12-18 18:11:19 -0500

    Allow multiple different users to upvote a meal.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,10 +1,10 @@
 class VotesController < ApplicationController
     def edit
         @meal = Meal.find(params[:id])
-        if @meal.votes.has_key?("user")
-            @meal.votes.delete("user")
+        if @meal.votes.has_key?(current_user.name)
+            @meal.votes.delete(current_user.name)
         else
-            @meal.votes["user"] = 1
+            @meal.votes[current_user.name] = 1
         end
         @meal.save
         redirect_to meals_path
