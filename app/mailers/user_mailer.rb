d456236cd5555caa735a10fe1df13f5500962e69
tommy <trzhang@mit.edu>
2012-12-24 19:29:39 -0500

    Renamed some stuff in UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,9 +15,9 @@ class UserMailer < ActionMailer::Base
   end
 
   def send_meal_event_emails()
-      MealEvent.all.each do |meal_event|
+      MealEvent.find_each do |meal_event|
           unless meal_event.reminded
-              if meal_event.time - Time.now <= meal_event.remind_in_advance * 60
+              if meal_event.time - Time.now <= meal_event.remind_in_advance * 3600
                   puts "Sending meal event mail"
                   UserMailer.meal_event_email(User.find(meal_event.cook), Meal.find(meal_event.meal), meal_event.date, meal_event.time).deliver
                   meal_event.update_attributes({reminded: true})
@@ -33,7 +33,7 @@ class UserMailer < ActionMailer::Base
   end
   
   def send_shopping_run_emails()
-      ShoppingRun.all.each do |shopping_run|
+      ShoppingRun.find_each do |shopping_run|
           unless shopping_run.reminded
               if shopping_run.date - Date.today <= shopping_run.remind_in_advance
                   puts "Sending shopping run mail"
@@ -44,24 +44,24 @@ class UserMailer < ActionMailer::Base
       end
   end
 
-  def need_people_email(users, meal_dates, shopping_runs)
-      @meal_dates = meal_dates
-      @shopping_runs = shopping_runs
+  def need_people_email(users, no_meal_dates, unshopped_meal_events)
+      @no_meal_dates = no_meal_dates
+      @unshopped_meal_events = unshopped_meal_events
       mail(to: users.map(&:email), subject: "Important: Need additional people")
   end
 
   def send_need_people_emails()
-      meal_dates = Set.new
-      shopping_runs = Set.new
+      no_meal_dates = Set.new
+      unshopped_meal_events = Set.new
       Date.today.upto(Date.today + 3) do |date|
           t = Time.new(date.year, date.month, date.day, 0, 0, 0, 0)
           if MealEvent.where(:date => t).empty?
-              meal_dates.add(date)
+              no_meal_dates.add(date)
           end
           MealEvent.where({:date => t, :status => nil}).each do |meal_event|
-              shopping_runs.add(meal_event)
+              unshopped_meal_events.add(meal_event)
           end
       end
-      UserMailer.need_people_email(User.find_each, meal_dates, shopping_runs).deliver
+      UserMailer.need_people_email(User.find_each, no_meal_dates, unshopped_meal_events).deliver
   end
 end
