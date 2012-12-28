91c8e3c2a887f8119737860adf3b246fa96c444f
kyc <kyc2915@mit.edu>
2012-12-28 14:18:23 -0600

    Add drop-down menu on Shopping Requests page to directly select a shopping run.

--- app/controllers/shopping_requests_controller.rb
+++ app/controllers/shopping_requests_controller.rb
@@ -1,18 +1,12 @@
 class ShoppingRequestsController < ApplicationController
     def new
-        @shopping_request = ShoppingRequest.new
-        @meal_event = params[:meal_event]
-    end
-
-    def create
         unless signed_in?
             return
         end
 
-        info = params[:shopping_request]
-        meal_event = MealEvent.find_by_id(info[:meal_event])
+        meal_event = MealEvent.find_by_id(params[:meal_event])
         unless has_shopper?(meal_event)
-            meal_event.shopping_run = ShoppingRun.find_by_id(info[:shopping_run])
+            meal_event.shopping_run = ShoppingRun.find_by_id(params[:shopping_run])
             meal_event.save
         end
         redirect_to shopping_runs_path
--- app/models/shopping_request.rb
+++
@@ -1,6 +0,0 @@
-class ShoppingRequest
-  include MongoMapper::Document
-
-  key :shopping_run, String # ShoppingRun.id
-  key :meal_event, String # MealEvent.id
-end
