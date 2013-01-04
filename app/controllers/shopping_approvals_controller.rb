8450040c506e3708dee3d6d81520e1d84a375e6a
kyc <kyc2915@mit.edu>
2013-01-04 15:29:48 -0600

    Allow admins to un-approve a shopping run.

--- app/controllers/shopping_approvals_controller.rb
+++ app/controllers/shopping_approvals_controller.rb
@@ -6,12 +6,23 @@ class ShoppingApprovalsController < ApplicationController
 
         shopping_run = ShoppingRun.find_by_id(params[:shopping_run])
         shopping_run.update_attributes(:approved => true)
-        log shopping_run
+        log shopping_run, 'approved'
         redirect_to shopping_runs_path
     end
 
-    def log(shopping_run)
-        MyLog.log "#{current_user.name} approved the shopping run by #{shopping_run.shopper.name} on #{shopping_run.date}"
+    def edit
+        unless is_admin?
+            return
+        end
+
+        shopping_run = ShoppingRun.find_by_id(params[:id])
+        shopping_run.update_attributes(:approved => false)
+        log shopping_run, 'un-approved'
+        redirect_to shopping_runs_path
+    end
+
+    def log(shopping_run, action)
+        MyLog.log "#{current_user.name} #{action} the shopping run by #{shopping_run.shopper.name} on #{shopping_run.date}"
     end
 end
 
