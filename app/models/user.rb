2e506ef7acf9674acd97d7c3f58b72db13dd90fb
kyc <kyc2915@mit.edu>
2013-01-06 11:25:24 -0600

    Add users summary information and link from admin view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,30 @@ class User
 
   before_create :create_remember_token
 
+  def liked_meals
+      Meal.all.select{ |meal| meal.voters.include?(id) }
+  end
+
+  def helped_cook
+      MealEvent.all.select { |meal_event| meal_event.helpers.include?(id) }
+  end
+
+  def helped_shop
+      ShoppingRun.all.select { |shopping_run| shopping_run.helpers.include?(id) }
+  end
+
+  def rsvps
+      MealEvent.all.select { |meal_event| meal_event.rsvps.include?(id) or meal_event.late_rsvps.include?(id) }
+  end
+
+  def attended
+      MealEvent.all.select { |meal_event| meal_event.customers.include?(id) }
+  end
+
+  def total_charge
+      'formula not finalized.'
+  end
+
   private
     def create_remember_token
         self.remember_token = SecureRandom.hex
